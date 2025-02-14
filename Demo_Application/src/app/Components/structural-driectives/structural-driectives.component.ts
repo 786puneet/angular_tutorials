f15394b2b39a41282f84c1ef9d25b2ae6e658ba0
786puneet <punitsood86@gmail.com>
2025-02-14 21:06:20 +0530

    created structural driective compoment

---
+++ Demo_Application/src/app/Components/structural-driectives/structural-driectives.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structural-driectives',
+  imports: [],
+  templateUrl: './structural-driectives.component.html',
+  styleUrl: './structural-driectives.component.css'
+})
+export class StructuralDriectivesComponent {
+
+}
