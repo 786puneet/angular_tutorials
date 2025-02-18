ab1914e455e7fe867ba55660c112865a1332e67c
786puneet <punitsood86@gmail.com>
2025-02-18 18:44:47 +0530

    Driective component

--- Demo_Application/src/app/Components/structural-driectives/structural-driectives.component.ts
+++ Demo_Application/src/app/Components/structural-driectives/structural-driectives.component.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-structural-driectives',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './structural-driectives.component.html',
   styleUrl: './structural-driectives.component.css'
 })
 export class StructuralDriectivesComponent {
+  isdiv1visiable:boolean=true;
+  toggelShow:boolean=true;
+  input1Value:string='';
+  input2Value:string='';
+  activeCheckbox:boolean=true;
+  selectCountry:string='';
+  allCity:string[]=['Mohali','Chandighar','Surat' , 'Panchakula'];
+  studentList:any[]=[
+    {id:1,name:'puneet',qualification:'bba', isActive:true},
+    {id:2,name:'karan aujala',qualification:'m.comm',isActive:true},
+    {id:3,name:'sidhu mossewala',qualification:'bca',isActive:false},
+    {id:4,name:'amrinder gill',qualification:'diploma',isActive:true}
+  ]
+  isShow(){
+    this.isdiv1visiable=true;
+  }
 
+  isHide(){
+    this.isdiv1visiable=false;
+  }
+
+  toggelBtn()
+  {
+   this.toggelShow=!this.toggelShow;
+  }
 }
--- Demo_Application/src/app/app.component.ts
+++ Demo_Application/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { EmployessComponent } from './Components/employess/employess.component';
-import { EmployessListComponent } from './Components/employess-list/employess-list.component';
-import { DataBindingComponent } from './Components/data-binding/data-binding.component';
+// import { EmployessComponent } from './Components/employess/employess.component';
+// import { EmployessListComponent } from './Components/employess-list/employess-list.component';
+// import { DataBindingComponent } from './Components/data-binding/data-binding.component';
+import { StructuralDriectivesComponent } from './Components/structural-driectives/structural-driectives.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,DataBindingComponent],
+  imports: [RouterOutlet,StructuralDriectivesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
