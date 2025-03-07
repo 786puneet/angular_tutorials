48f4f937ef1a7d183c45c3e4eb7fec1510bdb940
786puneet <punitsood86@gmail.com>
2025-03-07 07:48:02 +0530

    ng Template added

---
+++ Demo_Application/src/app/Components/ng-template/ng-template.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-template',
+  imports: [],
+  templateUrl: './ng-template.component.html',
+  styleUrl: './ng-template.component.css'
+})
+export class NgTemplateComponent {
+
+}
