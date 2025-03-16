56d3f9afc14e3b348bd5e8428adc0147320c3b6c
786puneet <punitsood86@gmail.com>
2025-03-16 15:17:24 +0530

    add api login

--- Demo_Application/src/app/Components/layout/layout.component.ts
+++ Demo_Application/src/app/Components/layout/layout.component.ts
@@ -9,6 +9,15 @@ import { Router, RouterLink, RouterOutlet } from '@angular/router';
 })
 export class LayoutComponent {
   router = inject(Router);
+  loggedUserData:any;
+ constructor(){
+  const loggedUser = localStorage.getItem('loginUser');
+  if(loggedUser != null)
+  {
+    this.loggedUserData = JSON.parse(loggedUser);
+  }
+ }
+
   logOut(){
   localStorage.removeItem('loginUser');
   this.router.navigateByUrl('login')
--- Demo_Application/src/app/Components/login/login.component.ts
+++ Demo_Application/src/app/Components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -12,19 +13,37 @@ import { Router } from '@angular/router';
 export class LoginComponent {
  
  userObj:any={
-  userName:'',
+  EmailId:'',
   password:''
  }
  router = inject(Router);
+ http = inject(HttpClient);
  onLogin(){
-  if (this.userObj.userName === 'admin' && this.userObj.password === '123456') {
-    alert("Login Successful");
-    localStorage.setItem('loginUser',this.userObj.userName);
+
+  // api or dynamic login
+this.http.post("https://projectapi.gerasim.in/api/UserApp/login",this.userObj).subscribe((res:any)=>{
+ if(res.result)
+ {
+   alert("Login Successful");
+   localStorage.setItem('loginUser', JSON.stringify(res.data));
     this.router.navigateByUrl('employess');
-  }
-  else
-  {
-    alert("login failed");
-  }
+ }
+ else
+ {
+  alert(res.message)
+ }
+})
+  
+
+  // static or hard code login
+  // if (this.userObj.userName === 'admin' && this.userObj.password === '123456') {
+  //   alert("Login Successful");
+  //   localStorage.setItem('loginUser',this.userObj.userName);
+  //   this.router.navigateByUrl('employess');
+  // }
+  // else
+  // {
+  //   alert("login failed");
+  // }
  }
 }
