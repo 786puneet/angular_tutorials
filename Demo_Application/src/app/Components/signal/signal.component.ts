0e02e90b72b95724ee3ebfac86ee04ebb72711f1
786puneet <punitsood86@gmail.com>
2025-03-17 08:35:49 +0530

    Signal completed

--- Demo_Application/src/app/Components/signal/signal.component.ts
+++ Demo_Application/src/app/Components/signal/signal.component.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { single } from 'rxjs';
 
 @Component({
   selector: 'app-signal',
   imports: [],
   templateUrl: './signal.component.html',
-  styleUrl: './signal.component.css'
+  styleUrl: './signal.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SignalComponent {
  firstName = signal('Angular 18');
+ rollNumber = signal<number>(33);
+ newVar:string='HTML';
+ cityName = signal(['Mumbai','Chandighar']);
+ userData = signal(
+  {
+    name:'puneet',
+    city:'Mohali'
+  }
+ )
+ constructor()
+ {
+  setTimeout(() => {
+    this.firstName.set('Dot Net');
+    this.newVar = 'Javascript';
+  }, 5000);
+ }
+
+ addCity(){
+  this.cityName.set([...this.cityName(), "Mohali"])
+ }
+
+ changeUserCity(){
+ this.userData.set({...this.userData(), city:'CHD.'})
+ }
 }
