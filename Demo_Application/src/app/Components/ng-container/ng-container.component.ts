28d5527d513965b5271da04ed65604a138f71584
786puneet <punitsood86@gmail.com>
2025-03-21 21:21:31 +0530

    added change detecation and zone.js

--- Demo_Application/src/app/Components/ng-container/ng-container.component.ts
+++ Demo_Application/src/app/Components/ng-container/ng-container.component.ts
@@ -1,26 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-ng-container',
   imports: [CommonModule],
   templateUrl: './ng-container.component.html',
-  styleUrl: './ng-container.component.css'
+  styleUrl: './ng-container.component.css',
+  changeDetection:ChangeDetectionStrategy.OnPush,
 })
 export class NgContainerComponent {
 
   isConatiner:boolean=false;
   http = inject(HttpClient);
-
+  valueText:string='html';
+ 
   userData:any[] = [];
   isAPI:boolean=false;
+  
+  private cdRef = inject(ChangeDetectorRef);
+
   allUserData(){
    this.isAPI=true;
-
+  
     this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((res:any)=>{
       this.userData = res;
       this.isAPI=false;
+      this.valueText='java';
+      setTimeout(() => {
+        this.cdRef.detectChanges();
+      }, 10000);
+     
     })
   }
 }
