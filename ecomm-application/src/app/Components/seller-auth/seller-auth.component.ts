b46387436a64c02839e93fd7437fb6af02d5963a
786puneet <punitsood86@gmail.com>
2025-03-27 16:47:08 +0530

    added header with routing

---
+++ ecomm-application/src/app/Components/seller-auth/seller-auth.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-seller-auth',
+  imports: [],
+  templateUrl: './seller-auth.component.html',
+  styleUrl: './seller-auth.component.css'
+})
+export class SellerAuthComponent {
+
+}
